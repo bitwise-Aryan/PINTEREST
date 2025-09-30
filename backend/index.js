3455b9e7c850dac008a4274da4140d8f722ea464
Aryan Kumar <aryansingh19july@gmail.com>
2025-09-30 15:39:50 +0530

    models,controllers

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ dotenv.config();
 //     return res.json("hello Aryan");
 // });
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 // app.use(cookieParser());
 // app.use(fileUpload());
